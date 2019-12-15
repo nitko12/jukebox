9a85e4dd7fe0a1dcf33aa24d050c9ec819ee37d7
nitko12 <nitko12@hotmail.com>
2019-12-15 13:58:27 +0100

    Fixed gray text

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-http.listen(3000, function() {
-    console.log("listening on *:3000");
+http.listen(8080, function() {
+    console.log("listening on *:8080");
 });
 
 const passportSocketIo = require("passport.socketio");
