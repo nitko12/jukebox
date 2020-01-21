e76f4120942b54dd0b891dad9c731ef342def8ed
nitko12 <nitko12@hotmail.com>
2020-01-21 11:52:11 +0100

    Wrong playing

--- player.js
+++ player.js
@@ -16,15 +16,58 @@ function shuffle(a) {
   return a;
 }
 
-module.exports = function(db, onplay) {
+module.exports = function (db, onplay) {
   class Player {
     constructor() {
       this.command = null;
       this.yt = null;
     }
-    async play(num) {
-      db.queue.getAll((err, data) => {
-        if (err) return console.log(err);
+    async play(num, arr) {
+      if (!arr) {
+        db.queue.getAll((err, data) => {
+          if (err) return console.log(err);
+          num %= data.length;
+          shuffle(data);
+          data.sort((a, b) => b.votes - a.votes);
+          if (data.length == 0) return;
+          let t = data[num].url;
+          console.log("Downloading: ", t);
+          onplay(data[num].id);
+          this.yt = ytdl(`https://www.youtube.com/watch?v=${t}`);
+          this.command = ffmpeg(this.yt)
+            .output("./temp/temp.wav")
+            .noVideo()
+            .format("wav")
+            .outputOptions("-ar", "44100")
+            .on("end", () => {
+              this.command = null;
+              this.yt = null;
+              wavplayer
+                .play({
+                  path: "./temp/temp.wav",
+                  sync: true
+                })
+                .then(
+                () => {
+                  console.log("The wav file played successfully.");
+                  fs.unlink("./temp/temp.wav", err => {
+                    if (err) console.log(err);
+                  });
+                  this.play(num + 1);
+                },
+                err => {
+                  console.log(err);
+                }
+                )
+                .catch(error => {
+                  console.error(error);
+                });
+            })
+            .run();
+        });
+        return;
+      } else {
+        let data = arr;
         num %= data.length;
         shuffle(data);
         data.sort((a, b) => b.votes - a.votes);
@@ -47,24 +90,23 @@ module.exports = function(db, onplay) {
                 sync: true
               })
               .then(
-                () => {
-                  console.log("The wav file played successfully.");
-                  fs.unlink("./temp/temp.wav", err => {
-                    if (err) console.log(err);
-                  });
-                  this.play(num + 1);
-                },
-                err => {
-                  console.log(err);
-                }
+              () => {
+                console.log("The wav file played successfully.");
+                fs.unlink("./temp/temp.wav", err => {
+                  if (err) console.log(err);
+                });
+                this.play(num + 1);
+              },
+              err => {
+                console.log(err);
+              }
               )
               .catch(error => {
                 console.error(error);
               });
           })
           .run();
-      });
-      return;
+      }
     }
 
     stop() {
